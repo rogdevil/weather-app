32e5e0029affe7fb6543963bd52ce3127014bbbb
Aryan <sep2001aryan@yahoo.com>
2020-05-13 18:59:44 +0530

    added translation api to translate various addresss to english

--- index.js
+++ index.js
@@ -1,41 +1,24 @@
 const request = require("request");
-const yargs = require("yargs");
+var prompts = require('prompts');
 const coordinateRequest = require("./playground/coordinateRequest");
 const addressRequest = require("./playground/addressRequest");
-argv = yargs
-    .options({
-        a: {
-            alias: "address",
-            describe: "address to fetch weather api",
-            string: true,
-        },
-        lng: {
-            alias: "longitude",
-            describe: "enter longitude (optional)",
-            number: true,
-        },
-        ltd: {
-            alias: "lattitude",
-            describe: "enter lattitude (optional)",
-            number: true,
-        },
-    })
-    .help()
-    .alias("help", "h").argv;
 
-//console.log(argv);
+//here we are taking input of address from the user
+for (let i = 0; i < 2; i++) {
 
-let address = encodeURIComponent(argv.a);
-let Longitude = argv.lng;
-let Lattitude = argv.ltd;
-// console.log(Longitude);
-// console.log(Lattitude);
-if (argv.a === undefined && argv.ltd == undefined && argv.lng === undefined) {
-    console.log(
-        "either enter the address or the corrdinates of the required palce"
-    );
-} else if (argv.address == undefined) {
-    coordinateRequest.coordinateRequest(Longitude, Lattitude);
-} else {
-    addressRequest.addressRequest(address);
+
+    let address;
+    (async () => {
+        const response = await prompts({
+            type: 'text',
+            name: 'Address',
+            message: 'Enter a valid pin code or Address-',
+        });
+        address = encodeURIComponent(response.Address);
+        console.log(response.Address);
+        addressRequest.addressRequest(address); // => { value: 24 }
+    })();
+
+
+
 }
--- playground/addressRequest.js
+++ playground/addressRequest.js
@@ -1,12 +1,13 @@
 const request = require("request");
 
 let addressRequest = (address) => {
-    request(
-        {
+    request({
             url: `https://geocode.search.hereapi.com/v1/geocode?q=${address}&limit=5&apiKey=xwqzui1BYBZZXx2JdbO5gZEy_T5ZXN6eVwdYyt9nXCE`,
             json: true,
         },
-        function (error, response, body) {
+        function(error, response, body) {
+            if (body.items[0] == undefined)
+                console.error("you entered a unknown address")
             let Lattitude = body.items[0].position.lat;
             let Longitude = body.items[0].position.lng;
             // console.log(JSON.stringify(body, undefined, 4));
@@ -17,35 +18,119 @@ let addressRequest = (address) => {
                     "unable to reach the server please check your internet connection"
                 );
             } else {
-                console.log(`Address: ${body.items[0].title}`);
-                //console.log(`Longitude: ${body.items[0].position.lng}`);
-                //console.log(`Lattitude: ${body.items[0].position.lat}`);
-                request(
-                    {
-                        url: `https://api.openweathermap.org/data/2.5/weather?lat=${Lattitude}&lon=${Longitude}&appid=151424fbe812fef9072439933e047003`,
-                        json: true,
-                    },
-                    (body, response, error) => {
-                        let temprature = response.body.main.temp - 273;
-                        let humidity = response.body.main.humidity;
-                        let description = response.body.weather.description;
-                        let winSpeed = response.body.wind.speed;
 
-                        // console.log(JSON.stringify(response, undefined, 4));
-                        console.log(description);
-                        console.log(`It feels like: ${temprature} Degree`);
-                        console.log(`Current Humidity: ${humidity}`);
-                        console.log(
-                            `Current Wind Speed: ${winSpeed} meter/sec`
+                // use of translation api to translate the address of various countries to english
+
+
+                let addr = body.items[0].title;
+
+                var options = {
+                    method: 'POST',
+                    url: 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect',
+                    headers: {
+                        'x-rapidapi-host': 'google-translate1.p.rapidapi.com',
+                        'x-rapidapi-key': '5de5cb5d74mshdf596aee5f99b98p11d49djsn2d113dcca3b5',
+                        'content-type': 'application/x-www-form-urlencoded',
+                        useQueryString: true
+                    },
+                    form: { q: addr }
+                };
+
+                request(options, function(error, response, body) {
+                    if (error) throw new Error(error);
+                    let bodydata = JSON.parse(body);
+                    // console.log("body data of of detected language is", bodydata);
+                    let languageCode = bodydata.data.detections[0][0].language;
+                    // console.log(typeof languageCode);
+                    if (languageCode == "en") {
+                        console.log(`Address: ${addr}`);
+                        //console.log(`Longitude: ${body.items[0].position.lng}`);
+                        //console.log(`Lattitude: ${body.items[0].position.lat}`);
+
+                        //here is our request for weather condition
+                        request({
+                                url: `https://api.openweathermap.org/data/2.5/weather?lat=${Lattitude}&lon=${Longitude}&appid=151424fbe812fef9072439933e047003`,
+                                json: true,
+                            },
+                            (body, response, error) => {
+                                let temprature = response.body.main.temp - 273;
+                                let humidity = response.body.main.humidity;
+                                let description = response.body.weather.description;
+                                let winSpeed = response.body.wind.speed;
+
+                                // console.log(JSON.stringify(response, undefined, 4));
+                                console.log(description);
+                                console.log(`It feels like: ${temprature} Degree`);
+                                console.log(`Current Humidity: ${humidity}`);
+                                console.log(
+                                    `Current Wind Speed: ${winSpeed} meter/sec`
+                                );
+                                if (response.body.rain) {
+                                    let rain = JSON.stringify(response.body.rain["3h"]);
+                                    console.log(`Rain in last 3 hours: ${rain} mm`);
+                                } else {
+                                    console.log("There is no rain in last 3 hours");
+                                }
+                            }
                         );
-                        if (response.body.rain) {
-                            let rain = JSON.stringify(response.body.rain["3h"]);
-                            console.log(`Rain in last 3 hours: ${rain} mm`);
-                        } else {
-                            console.log("There is no rain in last 3 hours");
-                        }
+                    } else {
+                        var options = {
+                            method: 'POST',
+                            url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+                            headers: {
+                                'x-rapidapi-host': 'google-translate1.p.rapidapi.com',
+                                'x-rapidapi-key': '5de5cb5d74mshdf596aee5f99b98p11d49djsn2d113dcca3b5',
+                                'content-type': 'application/x-www-form-urlencoded',
+                                useQueryString: true
+                            },
+                            form: { source: languageCode, q: addr, target: 'en' }
+                        };
+
+                        request(options, function(error, response, body) {
+                            if (error) throw new Error(error);
+
+                            let bodyData = JSON.parse(body);
+                            // console.log("body data of translated address is", bodyData);
+                            let translatedAddress = bodyData.data.translations[0].translatedText;
+                            addr = translatedAddress;
+
+
+                            console.log(`Address: ${addr}`);
+                            //console.log(`Longitude: ${body.items[0].position.lng}`);
+                            //console.log(`Lattitude: ${body.items[0].position.lat}`);
+
+                            //here is our request for weather condition
+                            request({
+                                    url: `https://api.openweathermap.org/data/2.5/weather?lat=${Lattitude}&lon=${Longitude}&appid=151424fbe812fef9072439933e047003`,
+                                    json: true,
+                                },
+                                (body, response, error) => {
+                                    let temprature = response.body.main.temp - 273;
+                                    let humidity = response.body.main.humidity;
+                                    let description = response.body.weather.description;
+                                    let winSpeed = response.body.wind.speed;
+
+                                    // console.log(JSON.stringify(response, undefined, 4));
+                                    console.log(description);
+                                    console.log(`It feels like: ${temprature} Degree`);
+                                    console.log(`Current Humidity: ${humidity}`);
+                                    console.log(
+                                        `Current Wind Speed: ${winSpeed} meter/sec`
+                                    );
+                                    if (response.body.rain) {
+                                        let rain = JSON.stringify(response.body.rain["3h"]);
+                                        console.log(`Rain in last 3 hours: ${rain} mm`);
+                                    } else {
+                                        console.log("There is no rain in last 3 hours");
+                                    }
+                                }
+                            );
+                        });
                     }
-                );
+
+
+
+                });
             }
         }
     );
